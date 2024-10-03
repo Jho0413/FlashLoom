de180dd939709978fc8b778b5099224e6ec94658
Jho0413 <ccjasonho@gmail.com>
2024-10-03 14:43:04 +0100

    Modified input arguments and handling diff methods for generation

--- server/app.py
+++ server/app.py
@@ -2,27 +2,27 @@ from flask import Flask, request, jsonify
 from youtube_url_generator import generate_from_youtube_url
 from pdf_generator import generate_from_pdf
 from text_generator import generate_from_text
-from langchain_huggingface.embeddings import HuggingFaceEmbeddings
-
 
 
 app = Flask(__name__)
 
 
-@app.route("/api/generate", methods=['GET'])
+@app.route("/api/generate", methods=['POST'])
 def generate():
     data = request.json
+    print(data)
+    method = data['method']
     message = data['message']
-    youtube_url = data['youtubeURL']
-    pdf_url = data['pdfURL']
-    if youtube_url:
+    if method == "Youtube":
+        youtube_url = data['youtube_url']
         response = generate_from_youtube_url(youtube_url, message)
-    elif pdf_url:
-        response = generate_from_pdf(pdf_url, message)
+    elif method == "PDF":
+        pdf_file = data['pdf_file']
+        response = generate_from_pdf(pdf_file, message)
     else:
         # basic generation
         response = generate_from_text(message)
-    return response
+    return jsonify(response)
 
 
 if __name__ == '__main__':

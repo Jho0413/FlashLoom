d45fd947bbed035273d3cff730aec321d6714c71
Jho0413 <ccjasonho@gmail.com>
2025-01-26 21:35:53 +0000

    Refactored: replaced components with reusable components. Added input checks for generating flashcards, and incrementing no. of generations for free plan when success

--- client/app/generate/flashcardForm.js
+++ client/app/generate/flashcardForm.js
@@ -7,6 +7,7 @@ import {
     Modal,
     Typography,
     CircularProgress,
+    Container,
 } from "@mui/material"
 import {
     TabContext,
@@ -16,206 +17,214 @@ import {
 import InputField from "../components/flashcards/inputField"
 import { useEffect, useState } from "react"
 import BuildIcon from '@mui/icons-material/Build';
-import { useUser } from "@clerk/nextjs"
+import { useUser } from "@clerk/nextjs";
+import LoadingModal from "../components/common/loadingModal";
+import ErrorModal from "../components/common/errorModal";
+import { useQueryClient } from "@tanstack/react-query";
+import PermissionDialog from "./permissionDialog"
 
 const FlashcardForm = ({ setFlashcards, setFlippedStates }) => {
-    const { user } = useUser();
-    const [formData, setFormData] = useState({});
-    const [file, setFile] = useState(null);
-    const [tabName, setTabName] = useState("Basic");
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState("");
-    const [openError, setOpenError] = useState(false);
+  const { user } = useUser();
+  const [formData, setFormData] = useState({ message: "" });
+  const [tabName, setTabName] = useState("Basic");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [inputError, setInputError] = useState("");
+  const [access, setAccess] = useState(true);
+  const queryClient = useQueryClient();
 
-    const handleTabChange = (event, newTab) => {
-        event.preventDefault();
-        setFormData({});
-        setTabName(newTab);
+  const handleTabChange = (event, newTab) => {
+    event.preventDefault();
+    switch (newTab) {
+      case "Basic":
+        setFormData({ message: "" });
+        break;
+      case "Youtube":
+        setFormData({ message: "", youtube_url: "" });
+        break;
+      case "PDF":
+        setFormData({ file: null, message: "" });
+        break;
+      default:
+        break;
     }
+    setInputError("");
+    setTabName(newTab);
+  }
 
-    const handleCloseError = () => {
-        setOpenError(false);
+  const handleFileUpload = (e) => {
+    e.preventDefault();
+    const file = e.target.files?.[0];
+    if (file && file.type && file.type === 'application/pdf') {
+      setFormData((prevFormData) => ({
+        ...prevFormData,
+        [e.target.name]: file,
+      }));
+      setInputError("");
+    } else {
+      setInputError("Please upload a valid PDF file");
     }
+  }
 
-    const handleFileUpload = (e) => {
-      e.preventDefault();
-      const file = e.target.files?.[0];
-      if (file && file.type && file.type === 'application/pdf') {
-        setFormData((prevFormData) => ({
-          ...prevFormData,
-          [e.target.name]: file,
-        }));
-      } else {
-        setError("Upload a valid pdf file");
-        setOpenError(true);
-        e.target.value = '';
-      }
-    }
-
-    const handleSubmit = async () => {
-        setLoading(true);
-        console.log({...formData});
-        console.log(file);
-        try {
-            const uploadData = new FormData();
-
-            // Append the form data to the FormData object
-            for (const key in formData) {
-              uploadData.append(key, formData[key]);
-            }
-            
-            // Add any other fields you want to include in the form data
-            uploadData.append('method', tabName);
-            const response = await fetch("/api/generate", {
-              method: "POST",
-              body: { ...uploadData },
-            });
-       
-            const data = await response.json();
-            if (!response.ok) {
-              throw new Error(data.error_message);
-            }
-            console.log("Generated flashcards:", data); 
-            console.log(data.flashcards);
-       
-            if (data.flashcards) {
-              setFlippedStates({});
-              setLoading(false);
-              setFlashcards(data.flashcards);
-            } else {
-              console.error("Unexpected response format:", data);
-              throw new Error("We have encountered internal errors. Please try again later.");
-            }
-        } catch (error) {
-            console.error("Error generating flashcards:", error);
-            setLoading(false);
-            setError(error.message);
-            setOpenError(true);
-        } finally {
-          setLoading(false);
+  const handleSubmit = async () => {
+    // form validation 
+    switch (tabName) {
+      case "Basic":
+        if (!formData.message.trim()) {
+          setInputError("Please fill out all required fields");
+          return;
         }
+        break;
+      case "Youtube":
+        if (!formData.youtube_url.trim()) {
+          setInputError("Please fill out all required fields");
+          return;
+        }
+        const regex = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/(watch\?v=|embed\/|v\/|.+\?v=)?([A-Za-z0-9_-]{11})$/;
+        if (!regex.test(formData.youtube_url)) {
+          setInputError("Please input a valid Youtube URL");
+          return;
+        }
+        break;
+      case "PDF":
+        if (!formData.file || formData.file.type !== "application/pdf") {
+          console.log("reached");
+          setInputError("Please upload a valid PDF file");
+          return;
+        }
+        break;
+      default: 
+        break;
     }
 
-    return (
-        <Box 
-          sx={{ 
-            display: "flex", 
-            flexDirection: "column", 
-            gap: 1,
-          }}
-        >
-          <TabContext value={tabName}>
-            <Box>
-              <TabList onChange={handleTabChange} variant="fullWidth" centered>
-                <Tab label="Basic" value="Basic" sx={{ color: 'white' }} />
-                <Tab label="Yotube" value="Youtube" sx={{ color: 'white' }} />
-                <Tab label="PDF" value="PDF" sx={{ color: 'white' }} />
-              </TabList>
-            </Box>
-            <TabPanel value="Basic">
-              <InputField 
-                name="message"
-                label="Enter your text"
-                rows={4}
-                value={formData}
-                setValue={setFormData}
-                required={true}
-              />
-            </TabPanel>
-            <TabPanel value="Youtube">
-              <InputField 
-                  name="youtube_url"
-                  label="Enter a youtube url"
-                  rows={1}
-                  value={formData}
-                  setValue={setFormData}
-                  required={true}
-              />
-              <InputField 
-                name="message"
-                label="Enter additional information for better generation (Optional)"
-                rows={4}
-                value={formData}
-                setValue={setFormData}
-                required={false}
-              />
-            </TabPanel>
-            <TabPanel value="PDF">
-              <form>
-                <input 
-                  type="file"
-                  name="file"
-                  onChange={handleFileUpload}
-                />
-              </form>
-              <InputField 
-                name="message"
-                label="Enter additional information for better generation (Optional)"
-                rows={4}
-                value={formData}
-                setValue={setFormData}
-                required={false}
-              />
-            </TabPanel>
-          </TabContext>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={handleSubmit}
-            sx={{
-                borderRadius: 10,
-            }}
-          >
-            Generate Flashcards
-          </Button>
+    // generating flashcards 
+    setLoading(true);
+    try {
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        body: JSON.stringify({ ...formData, method: tabName })
+      });
 
-          <Modal 
-            open={loading}
-            sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center"
-            }}
-          >
-            <Box 
-                sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: "center",
-                }}
-            >
-                <CircularProgress />
-            </Box>
-          </Modal>
-          <Modal 
-            open={openError}
-            onClose={handleCloseError}
-            sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-            }}
-          >
-            <Box 
-                sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    backgroundColor: "white",
-                    gap: 2,
-                    padding: 3,
-                    borderRadius: 2,
-                    maxWidth: "50%",
-                }}
-            >
-                <Typography variant="h6">{error}</Typography>
-                <BuildIcon />
-            </Box>
-          </Modal>
+      if (!response.ok) 
+        throw new Error("Unable to generate flashcards");
+
+      const data = await response.json();
+      const { flashcards } = data;
+      setFlashcards(flashcards);
+      setFlippedStates({}); 
+
+      // only updating generations if free plan
+      if (plan === "Free") {
+        queryClient.setQueryData([user.id, "subscriptionData"], (oldData) => {
+          return {
+            ...oldData, 
+            generations: oldData.generations + 1 
+          }
+        });
+      }
+    } catch (error) {
+      setError(true);
+    } finally {
+      setInputError("");
+      setLoading(false);
+    }
+  }
+
+  const renderTabContent = () => {
+    switch (tabName) {
+      case "Basic":
+        return (
+          <InputField 
+            name="message"
+            label="Enter your text"
+            rows={4}
+            value={formData}
+            setValue={setFormData}
+            required
+          />
+        );
+      case "Youtube":
+        return (
+          <Box>
+            <InputField 
+              name="youtube_url"
+              label="Enter a youtube url"
+              rows={1}
+              value={formData}
+              setValue={setFormData}
+              required
+            />
+            <InputField 
+              name="message"
+              label="Enter additional information for better generation (Optional)"
+              rows={4}
+              value={formData}
+              setValue={setFormData}
+            />
+          </Box>
+        );
+      case "PDF":
+        return (
+          <Box sx={{ display: "flex", flexDirection: "column", gap: 4 }}>
+            <form>
+              <input 
+                type="file"
+                name="file"
+                onChange={handleFileUpload}
+              />
+            </form>
+            <InputField 
+              name="message"
+              label="Enter additional information for better generation (Optional)"
+              rows={4}
+              value={formData}
+              setValue={setFormData}
+              required={false}
+            />
+          </Box>
+        );
+      default:
+        return null;
+    }
+  }
+
+  return (
+    <Box 
+      sx={{ 
+        display: "flex", 
+        flexDirection: "column", 
+        gap: 1,
+      }}
+    >
+      <TabContext value={tabName}>
+        <Box>
+          <TabList onChange={handleTabChange} variant="fullWidth" centered>
+            <Tab label="Basic" value="Basic" sx={{ color: 'white' }} />
+            <Tab label="Youtube" value="Youtube" sx={{ color: 'white' }} />
+            <Tab label="PDF" value="PDF" sx={{ color: 'white' }} />
+          </TabList>
         </Box>
-    )
+        <TabPanel value={tabName}>
+          <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+            {inputError && <Typography fontWeight="bold" color="error">{inputError}</Typography>}
+            {renderTabContent()}
+          </Box>
+        </TabPanel>
+      </TabContext>
+      <Container sx={{ display: "flex", justifyContent: "center" }}>
+        <Button
+          variant="contained"
+          color="primary"
+          // type="submit"
+          onClick={handleSubmit}
+        >
+          Generate Flashcards
+        </Button>
+      </Container>
+      <LoadingModal loading={loading} />
+      <ErrorModal error={error} setError={setError} />
+    </Box>
+  )
 }
 
 export default FlashcardForm;
